251db218aed03407b08bda22b3832b3f2d7ca7a4
Taro Aoki <aizu.s1230022@gmail.com>
2016-11-24 00:00:05 +0900

    [Add] online_battle.go and tests

---
+++ groovecoaster/online_battle.go
@@ -0,0 +1,37 @@
+package groovecoaster
+
+// Player is the structure that represents a part of players in online battle result
+type Player struct {
+	Name       string `json:"player_name"`
+	Prefecture string `json:"pref"`
+}
+
+// OnlineBattleSummary is the structure that represents a row of online battle list
+type OnlineBattleSummary struct {
+	Date     string
+	EID      string
+	Favorite StringToBool `json:"fav"`
+	MID      string
+	Rank     int
+	Star     int      `json:",string"`
+	Players  []Player `json:"detail"`
+}
+
+type onlineBattleSummary struct {
+	OnlineBattleSummary []*OnlineBattleSummary `json:"onlineBattleList"`
+}
+
+// OnlineBattleSummary fetches all online battle results
+func (p *APIClient) OnlineBattleSummary() ([]*OnlineBattleSummary, error) {
+	const uri = "mypage.groovecoaster.jp/sp/json/online_battle_list.php"
+
+	data, err := p.get(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	var obs onlineBattleSummary
+	p.unmarshal(data, &obs)
+
+	return obs.OnlineBattleSummary, nil
+}
---
+++ groovecoaster/online_battle_test.go
@@ -0,0 +1,29 @@
+package groovecoaster
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/jarcoal/httpmock"
+)
+
+func TestOnlineBattleSummary(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	data, err := ioutil.ReadFile("../tests/assets/online_battle_list.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	httpmock.RegisterResponder(
+		"GET",
+		scheme+"mypage.groovecoaster.jp/sp/json/online_battle_list.php",
+		httpmock.NewBytesResponder(200, data),
+	)
+
+	_, err = testClient.OnlineBattleSummary()
+	if err != nil {
+		t.Error(err)
+	}
+}
