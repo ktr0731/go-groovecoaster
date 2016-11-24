f2efb853886ddd2479768e9202201275dce8cacd
Taro Aoki <aizu.s1230022@gmail.com>
2016-11-25 00:47:28 +0900

    [Add] Failed tests for event_archive.go

--- groovecoaster/event_archive_test.go
+++ groovecoaster/event_archive_test.go
@@ -27,3 +27,51 @@ func TestEventArchive(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestEventArchive_BadStatus(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		scheme+"mypage.groovecoaster.jp/sp/json/event_data.php?event_id=28&old_flag=true",
+		httpmock.NewStringResponder(500, ""),
+	)
+
+	_, err := testClient.EventArchiveDetail(28)
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func TestEventArchive_EmptyBody(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		scheme+"mypage.groovecoaster.jp/sp/json/event_data.php?event_id=28&old_flag=true",
+		httpmock.NewStringResponder(200, ""),
+	)
+
+	_, err := testClient.EventArchiveDetail(28)
+	if err == nil {
+		t.Error(err)
+	}
+}
+
+func TestEventArchive_InvalidJSON(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"GET",
+		scheme+"mypage.groovecoaster.jp/sp/json/event_data.php?event_id=28&old_flag=true",
+		httpmock.NewStringResponder(200, `{"test": "test"}`),
+	)
+
+	_, err := testClient.EventArchiveDetail(28)
+	if err == nil {
+		t.Error(err)
+	}
+}
