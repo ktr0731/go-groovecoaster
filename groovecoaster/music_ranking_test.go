109ff74e78ff84642c45e34dcd72254393004f5c
Taro Aoki <aizu.s1230022@gmail.com>
2016-12-04 01:03:51 +0900

    [MOD] Clean some testsuit in music_ranking_test.go

--- groovecoaster/music_ranking_test.go
+++ groovecoaster/music_ranking_test.go
@@ -7,56 +7,56 @@ import (
 	"github.com/jarcoal/httpmock"
 )
 
+type testSuit struct {
+	assetName string
+	status    int
+	success   bool
+}
+
 func TestMusicRankingPageCount(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	data, err := ioutil.ReadFile("../tests/assets/music_ranking.json")
-	if err != nil {
-		t.Error(err.Error())
+	tests := []testSuit{
+		{
+			"../tests/assets/music_ranking.json",
+			200,
+			true,
+		},
+		{
+			"",
+			500,
+			false,
+		},
+		{
+			"",
+			200,
+			false,
+		},
 	}
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0",
-		httpmock.NewBytesResponder(200, data),
-	)
+	for _, test := range tests {
+		data := []byte("")
 
-	_, err = testClient.MusicRankingPageCount(290, Simple)
-	if err != nil {
-		t.Error(err)
-	}
-}
+		if test.assetName != "" {
+			var err error
+			data, err = ioutil.ReadFile(test.assetName)
+			if err != nil {
+				t.Error(err.Error())
+			}
+		}
 
-func TestMusicRankingPageCount_BadStatus(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
+		httpmock.RegisterResponder(
+			"GET",
+			scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0",
+			httpmock.NewBytesResponder(test.status, data),
+		)
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0",
-		httpmock.NewStringResponder(500, ""),
-	)
+		_, err := testClient.MusicRankingPageCount(290, Simple)
+		if err != nil && test.success {
+			t.Error(err)
+		}
 
-	_, err := testClient.MusicRankingPageCount(290, Simple)
-	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestMusicRankingPageCount_InvalidJSON(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0",
-		httpmock.NewStringResponder(200, `{"test": "test"}`),
-	)
-
-	_, err := testClient.MusicRankingPageCount(290, Simple)
-	if err == nil {
-		t.Error(err)
 	}
 }
 
@@ -64,51 +64,45 @@ func TestMusicRanking(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	data, err := ioutil.ReadFile("../tests/assets/music_ranking.json")
-	if err != nil {
-		t.Error(err.Error())
+	tests := []testSuit{
+		{
+			"../tests/assets/music_ranking.json",
+			200,
+			true,
+		},
+		{
+			"",
+			500,
+			false,
+		},
+		{
+			"",
+			200,
+			false,
+		},
 	}
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0&page=0",
-		httpmock.NewBytesResponder(200, data),
-	)
+	for _, test := range tests {
+		data := []byte("")
 
-	_, err = testClient.MusicRanking(290, Simple, 0)
-	if err != nil {
-		t.Error(err)
+		if test.assetName != "" {
+			var err error
+			data, err = ioutil.ReadFile(test.assetName)
+			if err != nil {
+				t.Error(err.Error())
+			}
+		}
+
+		httpmock.RegisterResponder(
+			"GET",
+			scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0&page=0",
+			httpmock.NewBytesResponder(test.status, data),
+		)
+
+		_, err := testClient.MusicRanking(290, Simple, 0)
+		if err != nil && test.success {
+			t.Error(err)
+		}
 	}
-}
 
-func TestMusicRanking_BadStatus(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0&page=0",
-		httpmock.NewStringResponder(500, ""),
-	)
-
-	_, err := testClient.MusicRanking(290, Simple, 0)
-	if err == nil {
-		t.Error(err)
-	}
-}
-
-func TestMusicRanking_InvalidJSON(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
-
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/score_ranking_bymusic_bydifficulty.php?music_id=290&difficulty=0&page=0",
-		httpmock.NewStringResponder(200, `{"test": "test"}`),
-	)
-
-	_, err := testClient.MusicRanking(290, Simple, 0)
-	if err == nil {
-		t.Error(err)
-	}
 }
