18f5f270425882c8510297810adc6a6926df3e43
Taro Aoki <aizu.s1230022@gmail.com>
2017-01-02 13:18:38 +0900

    [MOD] Integrate some test cases to one

--- groovecoaster/shop_musics_test.go
+++ groovecoaster/shop_musics_test.go
@@ -11,35 +11,36 @@ func TestShopMusics(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	data, err := ioutil.ReadFile("../tests/assets/shop_music_list.json")
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		assetName string
+		status    int
+		success   bool
+	}{
+		{"../tests/assets/shop_music_list.json", 200, true},
+		{"", 500, false},
 	}
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/shop_music_list.php",
-		httpmock.NewBytesResponder(200, data),
-	)
+	for _, test := range tests {
+		data := []byte("")
 
-	_, err = testClient.ShopMusics()
-	if err != nil {
-		t.Error(err)
-	}
-}
+		if test.assetName != "" {
+			var err error
 
-func TestShopMusics_BadStatus(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
+			data, err = ioutil.ReadFile(test.assetName)
+			if err != nil {
+				t.Error(err)
+			}
+		}
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/shop_music_list.php",
-		httpmock.NewStringResponder(500, ""),
-	)
+		httpmock.RegisterResponder(
+			"GET",
+			scheme+"mypage.groovecoaster.jp/sp/json/shop_music_list.php",
+			httpmock.NewBytesResponder(test.status, data),
+		)
 
-	_, err := testClient.ShopMusics()
-	if err == nil {
-		t.Error(err)
+		_, err := testClient.ShopMusics()
+		if err != nil && test.success {
+			t.Error(err)
+		}
 	}
 }
--- groovecoaster/shop_skins_test.go
+++ groovecoaster/shop_skins_test.go
@@ -11,35 +11,35 @@ func TestShopSkins(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	data, err := ioutil.ReadFile("../tests/assets/shop_skin_list.json")
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		assetName string
+		status    int
+		success   bool
+	}{
+		{"../tests/assets/shop_skin_list.json", 200, true},
+		{"", 500, false},
 	}
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/shop_skin_list.php",
-		httpmock.NewBytesResponder(200, data),
-	)
+	for _, test := range tests {
+		data := []byte("")
 
-	_, err = testClient.ShopSkins()
-	if err != nil {
-		t.Error(err)
-	}
-}
+		if test.assetName != "" {
+			var err error
 
-func TestShopSkins_BadStatus(t *testing.T) {
-	httpmock.Activate()
-	defer httpmock.DeactivateAndReset()
+			data, err = ioutil.ReadFile(test.assetName)
+			if err != nil {
+				t.Error(err)
+			}
+		}
+		httpmock.RegisterResponder(
+			"GET",
+			scheme+"mypage.groovecoaster.jp/sp/json/shop_skin_list.php",
+			httpmock.NewBytesResponder(test.status, data),
+		)
 
-	httpmock.RegisterResponder(
-		"GET",
-		scheme+"mypage.groovecoaster.jp/sp/json/shop_skin_list.php",
-		httpmock.NewStringResponder(500, ""),
-	)
-
-	_, err := testClient.ShopSkins()
-	if err == nil {
-		t.Error(err)
+		_, err := testClient.ShopSkins()
+		if err != nil && test.success {
+			t.Error(err)
+		}
 	}
 }
